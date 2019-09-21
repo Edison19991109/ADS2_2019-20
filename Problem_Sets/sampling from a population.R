a493a8e30db78b2440a38ba5927a26af901b30e5
Edison <jianning.18@intl.zju.edu.cn>
2019-09-21 21:39:16 +0800

    From sampling to population

---
+++ Problem_Sets/sampling from a population.R
@@ -0,0 +1,8 @@
+population <- rnorm(1e6,100,5)
+for (i in 1:1000) {
+  RandomPickUp<-sample(1:1e6,100)
+  popmean[i] <- round(mean(population[c(RandomPickUp)]),1)
+  popdev[i] <- round(sd(population[c(RandomPickUp)]),1)
+}
+hist(popmean)
+hist(popdev)
